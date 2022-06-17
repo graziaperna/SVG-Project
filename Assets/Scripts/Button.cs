21f7561c772a1f8f430c77f4f646c6beb109fe94
maria elena zaza <m.zaza16@studenti.uniba.it>
2022-06-17 10:53:54 +0200

    Migliorato script Move

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    public void Load()
+    {
+        
+    }
+
+    public void Exit()
+    {
+
+    }
+    public void GoOn()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
