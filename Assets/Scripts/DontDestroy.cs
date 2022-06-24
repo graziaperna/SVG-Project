31aa6924813669d627af8c0c82368c3e15476af8
graziaperna <g.perna4@studenti.uniba.it>
2022-06-24 14:06:11 +0200

    Inserita musica

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -5,9 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
+    public AudioClip[] clip;
+    private int nclip = 0;
+    private GameObject music;
+    CambioScena scena;
+    public GameObject objectEvent;
+
+
+    void Start()
+    {
+        
+    }
+
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("aren");
+        music = GameObject.FindWithTag("music");
+        scena = objectEvent.GetComponent<CambioScena>();
 
         if (objs.Length > 1)
         {
@@ -15,5 +29,31 @@ public class DontDestroy : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "SampleScene" && scena.changed == true)
+        {
+        nclip = 0;
+        changeMusic();
+        }
+        else if (SceneManager.GetActiveScene().name == "Tempio" && scena.changed == true)
+        {
+            nclip = 1;
+            changeMusic();
+        }
+        else if (SceneManager.GetActiveScene().name == "Casa Baba" && scena.changed == true)
+        {
+            nclip = 2;
+            changeMusic();
+        }
+    }
+
+    void changeMusic()
+    {
+            music.GetComponent<AudioSource>().clip = clip[nclip];
+            music.GetComponent<AudioSource>().GetComponent<AudioSource>().Play();
+            scena.setChanged(false);
     }
 }
